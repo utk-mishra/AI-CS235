5cbeb7d3c3820ec707c73508320986de3ff5ae4a
Utkarsh Mishra <54851908+utk-mishra@users.noreply.github.com>
2020-07-31 21:22:03 +0530

    Update genetic.cpp

--- genetic.cpp
+++ genetic.cpp
@@ -1,275 +1,282 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define pback push_back
-#define collection 8
-#define lli long long int
-#define integer int
+/*global section*/
 
-vector<vector<int>> form_collection;
-vector<lli> student_marks;
-set<lli> leader;
-vector<vector<int>> groups;
-void printinggroups();
-void  RANDOM(int key,int n,int i);
+#define ll long long 
+#define pb push_back
+#define V vector<ll>
+#define set vector<pair<ll,ll>>
+#define mp map<float,ll>
 
-int fit(int key,int n,int i);
-
-int fit2(int key,int mar);
-
-int crossmaking(int ease_1,int ease_2,int key,int g1);
-
+V vector1;
+vector<float>V1[10000];
+vector<float>final;
 
 int main()
 {
-        int m, k, marksobtained;
-        
-        cout<<"Enter Number of groups:";
-        cin>>k;
-        cout<<" "<<k<<endl;
 
-        cout<<"Enter value of n:";
-        cin>>m;
-        cout<<" "<<m<<endl;
-        
-        cout<<"Please Enter the marks obtained by students:";
-      //  for(int i=0;i<m;i++)
-    int j=0;
-    while(j<m)
-    {
-                cin>>marksobtained;
-                student_marks.pback(marksobtained);
-        j++;
-        }
-    cout<<endl;
-    cout<<"                               marks"<<"    gr representative"<<"\n";
-        vector<pair<int,int>> foo;
-        //for(int i=0;i<collection;i++)
-     j=0;
-        while(j<m)
-        {
-                mt19937 mt_rand ( time (NULL));
-                 RANDOM(k,m,j);
-                leader.clear();
-            j++;
-        }
-    
-    //for(int count1=0;count1<50;count1++)
-      int count1=0;
-      while(count1<50)  
-        {
-        for(int i=0;i<collection;i++)
-        {
-            int ze=fit(k,m,i);
-            foo.pback(make_pair(ze,i));
-        }
-        sort(foo.rbegin(),foo.rend());
-        int i=0;
-        mt19937 mt_rand (time(0));
-        for(i=0;i<collection;i++)
-        {
-            marksobtained=rand()%360;
-            if(marksobtained>=0 && marksobtained<=10)
-            {
-                int indexNumber=foo[0].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-            else if(marksobtained>=11 && marksobtained<=30)
-            {
-                int indexNumber=foo[1].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-            else if(marksobtained>=31 && marksobtained<=60)
-            {
-                int indexNumber=foo[2].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-            else if(marksobtained>=61 && marksobtained<=100)
-            {
-                int indexNumber=foo[3].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-            else if(marksobtained>=101 && marksobtained<=150)
-            {
-                int indexNumber=foo[2].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-            else if(marksobtained>=151 && marksobtained<=210)
-            {
-                int indexNumber=foo[2].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-            else if(marksobtained>=211 && marksobtained<=280)
-            {
-                int indexNumber=foo[2].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-            else if(marksobtained>=281 && marksobtained<=360)
-            {
-                int indexNumber=foo[2].second;
-                form_collection.pback(groups[indexNumber]);
-            }
-        }
-        auto ite=form_collection.begin();
-            int current_pop=0;
-            //for(i=0;i<4;i++)
-            j=0;
-            while(j<4)
-            {
-                groups[j].clear();
-                j++;
-            }
-            groups.clear();
-            int indexNumber1=0,indexNumber2=1;
-            for(;current_pop<collection;)
-            //while(current_pop<collection)
-            {
-                mt19937 mt_rand(time(NULL));
-                integer ease_1=rand()%4;
-                integer ease_2=rand()%4;
-                integer indexNumber=k-1;
-                indexNumber=rand()%indexNumber;
-                integer call1=crossmaking(ease_1,ease_2,k,indexNumber);
-                integer call2=crossmaking(ease_2,ease_1,k,indexNumber);
-                if( (call1==0) || (call2==0))
-                {
-                    groups.erase(groups.begin()+indexNumber1);
-                    continue;
-                }
-                else if((call1==1) && (call2==1))
-                {
-                        indexNumber1=indexNumber+2;
-                        indexNumber2=3;
-                        leader.clear();        
-                        current_pop=current_pop+2;
-                }
-            }
-        count1++;
-        }
-    printinggroups();
-}
+  ll i,j,n,d,m,k,l;
 
-
-
-void  RANDOM(int key,int n,int i)
-{
-        int ee,m;
-        mt19937 mt_rand (time(0));
-        //while(leader.size()!=k)
-        for(;leader.size()!=key;)
-        {
-                ee=rand()%n;
-                leader.insert(student_marks[ee]);
-        }
-        m=0;
-        vector<integer> temp;
-        for(auto j=leader.begin();j!=leader.end();j++)
-        {
-                temp.pback(*j);
-                m++;
-        }
-        groups.pback(temp);
-}
-int fit(int key,int n,int i)
-{
-    integer dis[key],fitnes=0,easy;
-    integer j=0;
-    //for(in=0;in<n;in++)
-        integer jn=0;
-    while(jn<n)
-    {
-        auto gr=groups[i].begin();
-        for(j=0;j<key;j++)
-        {
-            dis[j]=student_marks[jn]-(*gr);
-            dis[j]=dis[j]*dis[j];
-            gr++;
-        }
-        easy=1000020;
-       // for(j=0;j<key;j++)
-        while(j<key)
-        {
-            if(dis[j]<easy)
-            {
-                easy=dis[j];
-            }
-            j++;
-        }
-        fitnes=fitnes+easy;
-        jn++;
-    }
-    return fitnes;
-}
   
-  int crossmaking(int ease_1,int ease_2,int key,int g1)
-{
-    leader.clear();
-    int in=key-1, i;
-   // for(i=0;i<=g1;i++)
+  cout<<"please enter the number of groups: ";
+  cin>>k;
+  cout<<"please enter the number of students: ";
+  cin>>n;
+  cout<<"please enter the number of subjects: ";
+  cin>>m;
+ 
     i=0;
-    while(i<=g1)
-    {
-        leader.insert(form_collection[ease_1][i]);
-        i++;
-    }
-    //for(int j=i;j<key;j++)
-    integer j=i;
-    while(j<key)
-    {
-        leader.insert(form_collection[ease_2][j]);
-        j++;
-    }
-    if(leader.size()!=key)
-    {
-        return 0;
-    }
-    else if(leader.size()==key)
-    {
-    vector<integer> temporary1;
-    for(auto itc1=leader.begin();itc1!=leader.end();itc1++)
-    {
-         temporary1.pback(*itc1);
-    }
-    groups.pback(temporary1);
-    return 1;
-}
-} 
+    while(i<n)
 
-int fit2(int key,int mar)
-{
-int kkk=groups.size(),it_val,com=100000,dis,val;
-kkk--;
-auto ite=groups[kkk].begin();
-for(int i=0;i<key;i++)
-{
-it_val=*ite;
-dis=it_val-mar;
-dis=dis*dis;
-if(dis<com)
-{
-val=it_val;
-com=dis;
-}
-ite++;
-}
-return val;
-}
-void printinggroups()
-{
-integer nu=student_marks.size();
-integer ka=groups[0].size();
-integer student;
-auto student1=student_marks.begin();
-       // stu=*(stu1);
-int i=0;
-while(i<nu)
-//for(integer i=0;i<nu;i++)
-{
-//auto stu1=student_marks.begin();
-       student=*(student1);
-int gro=fit2(ka,student);
-cout<<"    marks obtained by student -- "<<student<<" -----> "<<gro<<endl;
-student1++;
-i++;
-}
+      { ll s=0;
+        cout<<"Marks of student "<<i+1<<": ";
+        j=0;
+        while(j<m)
+       
+        {
+           cin>>d;
+           s+=d;
+           j++;
+        }
+        s=s/m;
+        vector1.pb(s);
+        i++;
+      }
+ 
+
+      mt19937 mt_rand(time(NULL));
+
+      ll ps=80;
+
+      i=0;
+      while(i<ps)
+
+          {  
+            
+            ll A[10000]={0};
+            j=1;
+                     while(j<=k)
+              
+                    { 
+                        d = (mt_rand() % n);
+                        if(A[d]==0)
+                        { 
+                          A[d]+=1;
+                          V1[i].pb(vector1[d]);
+                        }
+                      else
+                      { 
+                        while(A[d]>=1)
+                        d = (mt_rand() % n);
+                       
+                        A[d]+=1;
+                        V1[i].pb(vector1[d]);
+                      }
+                   
+                        j++;
+                    }
+              
+            i++;
+          }
+ 
+      i=1;
+      while(i<=100)
+  
+          { 
+              set vec3;
+               V vec4,vec5,vec6;
+              V vec2[10000];
+              vector<float>V7[10000]; 
+              ll h=0;
+              while(h<ps)
+            
+            {  
+              j=0;
+              while(j<n)
+
+            { 
+              ll min = abs(V1[h][0]-vector1[j]);
+              ll min1 = 0;
+              l=1;while(l<k)
+              
+              {
+                if(abs(V1[h][l]-vector1[j])<min)
+                {
+                    min = abs(V1[h][l]-vector1[j]);
+                    min1 = l;
+                }
+                l++; 
+           }
+                vec2[h].pb(min1);
+            j++;
+          }
+          h++; 
+        } 
+            
+
+             ll s2=0;  
+
+             j=0;while(j<ps)
+            
+           { 
+              ll s=0;
+               for(l=0;l<n;l++)
+               s=s+(V1[j][vec2[j][l]]-vector1[l])*(V1[j][vec2[j][l]]-vector1[l]); 
+               s2=s2+s;  
+               vec3.pb({s,j}); 
+             j++;
+           }
+           //sorting of vec3
+           sort(vec3.begin(),vec3.end());  
+              
+           for(j=0;j<V1[vec3[0].second].size();j++)
+           {if(i==100)
+            final.pb(V1[vec3[0].second][j]);
+           }
+           reverse(vec3.begin(),vec3.end());
+
+              
+              
+           float s1=0;   
+           
+
+            j=1;while(j<=ps)
+          {
+           s1+=j;
+           j++;
+         }   
+            j=1;while(j<=ps)
+         
+           { float d=(j/s1)*100;
+             ll d1=ceil(d);
+             
+             if((d1-d)>0.5)
+             vec4.pb(floor(d));
+             else
+             vec4.pb(ceil(d)); 
+             j++;  
+           }
+           
+
+           vec5.pb(1);
+           ll z=vec4[0];   
+               j=1;while(j<vec4.size())
+         
+           {vec5.pb(z+1);
+            z=z+vec4[j];
+            j++;
+           }
+              
+          j=1;while(j<=ps)
+
+           { d = (rand() % 100)+1;
+             
+             ll pos=lower_bound(vec5.begin(), vec5.end(), d)-vec5.begin();
+             if(pos!=0)
+             pos=pos-1;    
+             vec6.pb(vec3[pos].second);
+          j++; }
+
+         
+           ll z1=0;   
+            j=1;while(j<=(ps/2)) 
+           
+           { d = (rand() % ps);
+             
+             ll d1 =(rand() % ps);
+             
+             ll cnt=0;
+             
+            
+              for(;d1==d && cnt<=60;)
+             {d1 = (rand() % ps);
+              cnt++;
+             }
+            
+            
+             ll d2 = (rand() % k);
+
+             ll d3 = (rand() % 2);
+
+              mp B,C;
+            
+            if(d3==0)
+            { l=0;while(l<=d2)
+          
+              { V7[z1].pb(V1[vec6[d1]][l]);
+                V7[z1+1].pb(V1[vec6[d]][l]);
+              l++;}
+             l=d2+1;
+             while(l<k)
+
+             {V7[z1].pb(V1[vec6[d]][l]);
+              V7[z1+1].pb(V1[vec6[d1]][l]);
+             l++;}
+            }
+            else
+            { 
+              l=d2+1;
+             while(l<k)
+
+              { V7[z1].pb(V1[vec6[d1]][l]);
+                V7[z1+1].pb(V1[vec6[d]][l]);
+             l++;
+              }
+             l=0;while(l<=d2)
+         
+             {V7[z1].pb(V1[vec6[d]][l]);
+              V7[z1+1].pb(V1[vec6[d1]][l]);
+             l++;}
+            }     
+            
+
+            z1+=2;
+          j++; }
+           h=0;
+           while(h<ps)
+
+          {
+            V1[h].clear();
+            V1[h]=V7[h];
+           h++;
+          }
+              
+        i++; 
+        }
+            
+   V groups[k]; 
+
+   
+   for(ll j=0; j<vector1.size();j++)
+   { ll min,min1;
+     for(ll i=0; i<final.size(); i++)
+    { ll res = abs(final[i]-vector1[j])*abs(final[i]-vector1[j]);
+       if(i==0)
+     {min = res;
+      min1 = i;
+     }
+      if(res < min)
+      { min = res;
+        min1 = i;
+      }
+    }
+    groups[min1].pb(j+1);
+   }
+    
+    i=0;
+    while(i<final.size())
+
+   { cout<<"Group "<<i+1<<": ";
+    j=0;while(j<groups[i].size())
+    
+     {cout<<groups[i][j]<<" ";
+    j++;}
+     cout<<"\n";
+  i++; }
+     
+       
+
+  return 0;
+    
 }
